ebfee5061cf51f8e7756bc8e30e8f9eb49b11272
Kartik Saraf <kartik.saraf@materialplus.io>
2024-12-02 18:11:11 +0530

    More boilerplate code

--- app/src/main/java/saraf/kartik/moodlog/MainActivity.kt
+++ app/src/main/java/saraf/kartik/moodlog/MainActivity.kt
@@ -1,29 +1,86 @@
 package saraf.kartik.moodlog
 
+import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 //Setup Theme
 @Composable
 fun MoodLogTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colorScheme = lightColorScheme(),
+        colorScheme = lightColorScheme(
+            primary = Color.White,
+            secondary = Color.Black,
+            background = Color.White,
+            surface = Color.White,
+            onPrimary = Color.Black,
+            onSecondary = Color.White,
+            onBackground = Color.Black,
+            onSurface = Color.Black
+        ),
         typography = androidx.compose.material3.Typography(),
         content = content
     )
 }
 
+@Composable
+fun Hello() {
+    Text("hello moodlog")
+}
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Moodlog)
         super.onCreate(savedInstanceState)
         setContent {
-            
+            MoodLogTheme {
+                Hello()
+            }
+        }
+        hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+
+        //Hides the ugly action bar at the top
+        actionBar?.hide()
+
+        //Hide the status bars
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        } else {
+            window.insetsController?.apply {
+                hide(WindowInsets.Type.statusBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
         }
     }
 }
+
+
+@Preview(showBackground = true, device = Devices.PIXEL_7)
+@Composable
+fun HelloPreview() {
+    MoodLogTheme {
+        Hello()
+    }
+}
+
